9dee7a9e173fb61976c21f637e68a25a5fe4317c
Dent <153186003+Phelinos@users.noreply.github.com>
2024-03-02 16:30:46 +0000

    Create script.js

---
+++ script.js
@@ -0,0 +1,37 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const testimonials = document.querySelectorAll(".testimonial");
+    const prevButton = document.querySelector(".prev");
+    const nextButton = document.querySelector(".next");
+
+    let currentTestimonialIndex = 0;
+
+    function showTestimonial(index) {
+        testimonials.forEach((testimonial, i) => {
+            testimonial.style.display = "none";
+            testimonial.style.opacity = 0.8;
+        });
+        testimonials[index].style.display = "block";
+        testimonials[index].style.opacity = 1;
+    }
+
+    function nextTestimonial() {
+        currentTestimonialIndex++;
+        if (currentTestimonialIndex >= testimonials.length) {
+            currentTestimonialIndex = 0;
+        }
+        showTestimonial(currentTestimonialIndex);
+    }
+
+    function prevTestimonial() {
+        currentTestimonialIndex--;
+        if (currentTestimonialIndex < 0) {
+            currentTestimonialIndex = testimonials.length - 1;
+        }
+        showTestimonial(currentTestimonialIndex);
+    }
+
+    showTestimonial(currentTestimonialIndex);
+
+    nextButton.addEventListener("click", nextTestimonial);
+    prevButton.addEventListener("click", prevTestimonial);
+});
