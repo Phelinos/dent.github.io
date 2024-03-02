1a94662f8bead4a9f0579cbec9a91d5b86fb7efb
Dent <153186003+Phelinos@users.noreply.github.com>
2024-03-02 15:20:33 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,1 @@
-function enlargeImage(element) {
-  element.getElementsByTagName("img")[0].style.transform = "scale(1.1)";
-}
 
-function resetImage(element) {
-  element.getElementsByTagName("img")[0].style.transform = "scale(1)";
-}
