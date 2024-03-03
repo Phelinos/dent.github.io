60a80fb96872ebf0b2071d00a1220e001954b9b7
Dent <153186003+Phelinos@users.noreply.github.com>
2024-03-03 10:04:40 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
-// Function to create a circle element with random speed
+function getRandom(min, max) {
+    return Math.random() * (max - min) + min;
+}
+
 function createCircle() {
     const circle = document.createElement('div');
     circle.classList.add('circle');
-    circle.style.left = '0px'; // Start all circles from the left edge
+    circle.style.left = '0px';
     circle.style.top = getRandom(0, window.innerHeight) + 'px';
-    const speed = getRandom(2, 6); // Random speed between 2s and 6s
-    circle.style.animationDuration = speed + 's'; // Set animation duration
-    circle.style.animationTimingFunction = 'linear'; // Set animation timing function to linear
-    document.getElementById('circles-container').appendChild(circle);
+    const speed = getRandom(2, 6);
+    circle.style.animationDuration = speed + 's';
+    document.querySelector('.background').appendChild(circle);
 }
 
-// Create circles at regular intervals
-setInterval(createCircle, 2000); // Create a new circle every 2 seconds
+setInterval(createCircle, 2000);
